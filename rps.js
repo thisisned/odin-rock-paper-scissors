8ee52e3979cccb3533ae6d5bebd80d090246dd91
Ned <ned.wilson.eames@gmail.com>
2022-04-27 18:00:25 +0100

    Added game loop and score feedback

--- rps.js
+++ rps.js
@@ -19,15 +19,40 @@ function humanPlay() {
 
 function playRound(human, computer) {
     let result;
-    let win = "You win! " + human + " beats " + computer + ".";
-    let lose = "You lose! " + computer + " beats " + human + ".";
-    let tie = "It's a tie! You both chose " + human;
-    if (human === "rock" && computer === "scissors") result = win;
-    else if (human === "paper" && computer === "rock") result = win;
-    else if (human === "scissors" && computer === "paper") result = win;
-    else if (human === computer) result = tie;
-    else result = lose;
-    console.log(result);
+    if (human === "rock" && computer === "scissors") result = "win";
+    else if (human === "paper" && computer === "rock") result = "win";
+    else if (human === "scissors" && computer === "paper") result = "win";
+    else if (human === computer) result = "tie";
+    else result = "lose";
+    switch (result) {
+        case "win":
+            console.log("You win! " + human + " beats " + computer + ".");
+            break;
+        case "lose":
+            console.log("You lose! " + computer + " beats " + human + ".");
+            break;
+        case "tie":
+            console.log("It's a tie! You both chose " + human);
+            break; 
+    }
+    return result;
 }
 
-playRound(humanPlay(), computerPlay());
+function game() {
+    let playerScore = 0;
+    let compScore = 0;
+    for (let round = 0; round < 5; round++) {
+        console.log("Current score: Human: " + playerScore + " | Computer: " + compScore);
+        let roundResult = playRound(humanPlay(), computerPlay());
+        if (roundResult === "win") playerScore++;
+        else if (roundResult === "lose") compScore++;
+    }
+    console.log("---------------------");
+    console.log("Final result:");
+    if (playerScore === compScore) console.log("It's a tie! " + playerScore + " all.");
+    else {
+        playerScore > compScore ? console.log("You win! " + playerScore + " to " + compScore) : console.log("You lose! " + compScore + " to " + playerScore);
+    }
+}
+
+game();
